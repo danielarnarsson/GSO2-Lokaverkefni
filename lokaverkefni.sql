e0009fd4a356db692304ea217820bc07b7e0f87a
danielarnarsson <danielar216@nemi.tskoli.is>
2017-11-23 22:49:20 +0000

    Completed inserting data into all tables

--- lokaverkefni.sql
+++ lokaverkefni.sql
@@ -10,7 +10,7 @@ MEM_STREET VARCHAR(32) NOT NULL,
 MEM_CITY VARCHAR(32) NOT NULL,
 MEM_STATE CHAR(2) NOT NULL,
 MEM_ZIP CHAR(5)NOT NULL,
-MEM_BALANCE DECIMAL(5,2)
+MEM_BALANCE DECIMAL(6,2)
 );
 
 CREATE TABLE RENTAL (
@@ -31,7 +31,7 @@ CREATE TABLE MOVIE (
 MOVIE_NUM INT NOT NULL PRIMARY KEY,
 MOVIE_TITLE VARCHAR(255) NOT NULL,
 MOVIE_YEAR YEAR,
-MOVIE_COST DECIMAL(3,2) NOT NULL,
+MOVIE_COST DECIMAL(5,2) NOT NULL,
 MOVIE_GENRE VARCHAR(100),
 PRICE_CODE VARCHAR(1),
 FOREIGN KEY (PRICE_CODE) REFERENCES PRICE(PRICE_CODE)
@@ -47,7 +47,7 @@ FOREIGN KEY (MOVIE_NUM) REFERENCES MOVIE(MOVIE_NUM)
 CREATE TABLE DETAILRENTAL (
 RENT_NUM INT NOT NULL,
 VID_NUM INT NOT NULL,
-DETAIL_FEE DECIMAL(2,2),
+DETAIL_FEE DECIMAL(5,2),
 DETAIL_DUEDATE DATE,
 DETAIL_RETURNDATE DATE,
 DETAIL_DAILYLATEFEE DECIMAL(5,2),
@@ -61,47 +61,86 @@ PRIMARY KEY (RENT_NUM, VID_NUM)
 #2
 
 INSERT INTO MEMBERSHIP (MEM_NUM,MEM_FNAME,MEM_LNAME,MEM_STREET, MEM_CITY,MEM_STATE,MEM_ZIP,MEM_BALANCE) VALUES
-(102,'Tami','Dawson','2632 Takli Circle', 'Norene', 'TN', '37136', 11),
+(102,'Tami', 'Dawson', '2632 Takli Circle', 'Norene', 'TN', '37136', 11),
 (103, 'Curt', 'Knight', '4025 Cornell Court', 'Flatgap', 'KY', '41219', 6),
-(104, 'Jamal','Melendez', '788 East 145th Avenue', 'Quebeck', 'TN', '38579', 0),
-(105, 'Iva','Mcclain', '6045 Musket Ball Circle', 'Summit', 'KY', '42783', 15),
-(106,'Miranda', 'Parks', '4469 Maxwell Place', 'Germantown','TN', '38183', 0),
-(107,'Rosario','Elliott','7578 Danner Avenue','Columbia','TN','38402',5),
-(108,'Mattie','Guy','40390 Evergreen Street','Lily', 'KY', '40740', 0 ),
-(109,'Clint','Ochoa', '1711 Elm Street', 'Greeneville','TN', '37745',10),
-(110,'Lewis','Rosales','4524 Southwind Circle','Counce','TN','38326',0),
-(111,'Stacy','Mann', '2789 East Cook Avenue','Murfreesboro', 'TN','37132',8),
-(112, 'Luis', 'Trujillo', '7267 Melvin Avenue', 'Heiskell','TN','37754',3),
-(113, 'Minnie','Gonzales','6430 Vasili Drive','Williston','TN','38076',0);
+(104, 'Jamal', 'Melendez', '788 East 145th Avenue', 'Quebeck', 'TN', '38579', 0),
+(105, 'Iva', 'Mcclain', '6045 Musket Ball Circle', 'Summit', 'KY', '42783', 15),
+(106, 'Miranda', 'Parks', '4469 Maxwell Place', 'Germantown', 'TN', '38183', 0),
+(107, 'Rosario', 'Elliott','7578 Danner Avenue', 'Columbia', 'TN', '38402', 5),
+(108, 'Mattie', 'Guy', '40390 Evergreen Street', 'Lily', 'KY', '40740', 0),
+(109, 'Clint', 'Ochoa', '1711 Elm Street', 'Greeneville', 'TN', '37745', 10),
+(110, 'Lewis', 'Rosales', '4524 Southwind Circle', 'Counce', 'TN', '38326', 0),
+(111, 'Stacy', 'Mann', '2789 East Cook Avenue', 'Murfreesboro', 'TN', '37132', 8),
+(112, 'Luis', 'Trujillo', '7267 Melvin Avenue', 'Heiskell','TN', '37754', 3),
+(113, 'Minnie','Gonzales', '6430 Vasili Drive', 'Williston', 'TN', '38076', 0);
 
 INSERT INTO RENTAL (RENT_NUM, RENT_DATE, MEM_NUM) VALUES
-(1001,'2009-03-01',103),
-(1002,'2009-03-01',105),
-(1003,'2009-03-02',102),
-(1004,'2009-03-02',110),
-(1005,'2009-03-02',111),
-(1006,'2009-03-02',107),
-(1007,'2009-03-02',104),
-(1008,'2009-03-03',105),
-(1009,'2009-03-03',111);
+(1001, '2009-03-01', 103),
+(1002, '2009-03-01', 105),
+(1003, '2009-03-02', 102),
+(1004, '2009-03-02', 110),
+(1005, '2009-03-02', 111),
+(1006, '2009-03-02', 107),
+(1007, '2009-03-02', 104),
+(1008, '2009-03-03', 105),
+(1009, '2009-03-03', 111);
+
+INSERT INTO PRICE VALUES
+('1', 'Standard', 2, 1),
+('2', 'New Release', 3.5, 3),
+('3', 'Discount', 1.5, 1),
+('4', 'Weekly Special', 1, .5);
+
+INSERT INTO MOVIE VALUES
+(1234, "The Cesar Family Christmas", 2009, 39.95, 'FAMILY', 2),
+(1235, "Smokey Mountain Wildlife", 2006, 59.95, 'ACTION', 1),
+(1236, "Richard Goodhope", 2010, 59.95, 'DRAMA', 2),
+(1237, "Beatnik Fever", 2009, 29.95, 'COMEDY', 2),
+(1238, "Constant Companion", 2010, 89.95, 'DRAMA', null),
+(1239, "Where Hope Dies", 2000, 25.49, 'DRAMA', 3),
+(1245, "Time to Burn", 2007, 45.49, 'ACTION', 1),
+(1246, "What He Doesn't Know", 2008, 58.29, 'COMEDY', 1);
+
+INSERT INTO VIDEO VALUES
+(34341, '2009-01-22', 1235),
+(34342, '2009-01-22', 1235),
+(34366, '2009-03-11', 1236),
+(34367, '2009-03-11', 1236),
+(34368, '2009-03-11', 1236),
+(34369, '2009-03-11', 1236),
+(44392, '2010-10-21', 1237),
+(44397, '2010-10-21', 1237),
+(54321, '2010-06-18', 1234),
+(54324, '2010-06-18', 1234),
+(54325, '2010-06-18', 1234),
+(59237, '2011-02-14', 1237),
+(61353, '2008-01-28', 1245),
+(61354, '2008-01-28', 1245),
+(61367, '2010-07-30', 1246),
+(61369, '2010-07-30', 1246),
+(61388, '2009-01-25', 1239);
 
 INSERT INTO DETAILRENTAL VALUES
-(1001,34342,2,'2009-03-04','2009-03-02',1),
-(1001,61353,2,'2009-03-04','2009-03-03',1),
-(1002,59237,3.5,'2009-03-04','2009-03-04',3),
-(1003,54325,3.5,'2009-03-04','2009-03-09',3),
-(1003,61369,2,'2009-03-06','2009-03-09',1),
-(1003,61388,0,'2009-03-06','2009-03-09',1),
-(1004,34341,2,'2009-03-07','2009-03-07',1),
-(1004,34367,3.5,'2009-03-05','2009-03-07',3),
-(1004, 44392,3.5,'2009-03-05','2009-03-07',3),
-(1005,34342,2,'2009-03-07','2009-03-05',1),
-(1005, 44397,3.5,'2009-03-05','2009-03-05',3),
-(1006, 34366,3.5,'2009-03-05' ,'2009-03-04',3),
-(1006,61367,2,'2009-03-07',null,1),
-(1007,34368,3.5,'2009-03-05',null,3),
-(1008,34369,3.5,'2009-03-05','2009-03-05',3),
-(1009,54324 ,3.5,'2009-03-04',null,3);
-#DROP DATABASE 1907002160_Lokaverkefni;
+(1001, 34342, 2, '2009-03-04', '2009-03-02', 1),
+(1001, 34366, 3.5, '2009-03-04', '2009-03-02', 3),
+(1001, 61353, 2, '2009-03-04', '2009-03-03', 1),
+(1002, 59237, 3.5, '2009-03-04', '2009-03-04', 3),
+(1003, 54325, 3.5, '2009-03-04', '2009-03-09', 3),
+(1003, 61369, 2, '2009-03-06', '2009-03-09', 1),
+(1003, 61388, 0, '2009-03-06', '2009-03-09', 1),
+(1004, 34341, 2, '2009-03-07', '2009-03-07', 1),
+(1004, 34367, 3.5, '2009-03-05', '2009-03-07', 3),
+(1004, 44392, 3.5, '2009-03-05', '2009-03-07', 3),
+(1005, 34342, 2, '2009-03-07', '2009-03-05', 1),
+(1005, 44397, 3.5, '2009-03-05', '2009-03-05', 3),
+(1006, 34366, 3.5, '2009-03-05', '2009-03-04', 3),
+(1006, 61367, 2, '2009-03-07', null, 1),
+(1007, 34368, 3.5, '2009-03-05', null, 3),
+(1008, 34369, 3.5, '2009-03-05', '2009-03-05', 3),
+(1009, 54324, 3.5, '2009-03-05', null, 3);
+
+
+
+
 
 DROP DATABASE 1907002160_lokaverkefni;
